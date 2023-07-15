2a00c54244b82fd5da96eca8ac61b5e5d87e1e90
abc <abc@gmail.com>
2023-07-15 17:53:01 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
+import React,{useState} from 'react';
+import {FaFacebook, FaGoogle,FaTwitter} from 'react-icons/fa'
 import './App.css';
-
+import imgs from './#03.svg'
 function App() {
+  let [name, setName] = useState("");
+  let [mail, setMail] = useState("");
+  let [password, setPassword] = useState("");
+  let [confirmPassword, setConfirmpassword] = useState("");
+  let [suss, setSucessor] = useState("");
+  let [err, setError] = useState("");
+ 
+  const checkdetails = () =>{
+      if(name == "" || mail == "" || password == "" || confirmPassword == ""){
+        setSucessor("")
+        setError("Error: Please make sure your all the details are filled!")
+      }
+      else if(password !== confirmPassword){
+        setSucessor("")
+        setError("Error: Please make sure your password and confirm password match!")
+      }
+      else{
+        setError("")
+        setSucessor("Successfully Created!")
+      }
+  }
+
   return (
+    <div className='full'>
+    <div className='desighside'>
+      <p className='leftext'>Find 3D Objects, Mockups and Ilustration here</p>
+      <img src={imgs}></img>
+    </div>  
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Create Account</h1>
+      <div className='container'>
+      <div className='links'>
+        <div className='golink'>
+         <FaGoogle className='google'></FaGoogle>
+         <p>Sign up with Google</p>
+         </div>
+         <div className='falink'>
+         <FaFacebook className='facebook'></FaFacebook>
+         <p>Sign up with facebook</p>
+         </div>
+      </div>
+      <h1 className='or'>-OR-</h1>
+      <div className='inputs'>
+        <input className='name' type='text' placeholder='Full Name' onChange={(e)=> setName(e.target.value)}></input>
+        <input className='mail' type='email' placeholder='Email Address' onChange={(e)=> setMail(e.target.value)}></input>
+        <input className='pass' type='text' placeholder='Password' onChange={(e)=> setPassword(e.target.value)}></input>
+        <input className='conPass' type='text' placeholder='Confirm Password' onChange={(e)=> setConfirmpassword(e.target.value)}></input>
+        <button type='submit' onClick={checkdetails}>Create Account</button>
+      </div>
+      <p className='error'>{err}</p>
+      <p className='success'>{suss}</p>
+      </div>
+    </div>
     </div>
   );
 }
